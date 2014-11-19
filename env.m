b252cc92b13d66c715ef9c8185d28a6385994d95
Leslie Hsu <hsu.leslie@gmail.com>
2014-11-18 23:53:18 -0500

    additional comment
    seemingly useless i

--- env.m
+++ env.m
@@ -5,7 +5,7 @@ function y=env(y);
 % uses hilbert function. tracks the power of the signal. 
 % has phase information, preserves frequency information.
 
-i=sqrt(-1);
+i=sqrt(-1); % just FYI I guess
 
 yh=hilbert(y); % NOTE MATLAB MAKES THE ANAYLTIC SIGNAL IN ONE GO
 y=abs(yh);
